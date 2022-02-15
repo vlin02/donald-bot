cb1f92448b61b17683b6f99c3e315e736c686450
Victor Lin <vvictor.llin@gmail.com>
2022-02-15 06:54:24 -0800

    feat: add basic login

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log('hi');
+Object.defineProperty(exports, "__esModule", { value: true });
+const discord_js_1 = require("discord.js");
+require("dotenv/config");
+// Create a new client instance
+const client = new discord_js_1.Client({ intents: [discord_js_1.Intents.FLAGS.GUILDS] });
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+    console.log('Ready!');
+});
+// Login to Discord with your client's token
+client.login(process.env.DISCORD_BOT_TOKEN);
---
+++ src/env.d.ts
@@ -0,0 +1,3 @@
+declare namespace NodeJS {
+    export interface ProcessEnv extends Record<'DISCORD_BOT_TOKEN', string> {}
+}
